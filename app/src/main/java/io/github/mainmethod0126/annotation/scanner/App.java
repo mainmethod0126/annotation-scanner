96fabfd0139f031c641729551937d785b521df13
wusub.shin@softcamp.co.kr <wusub.shin@softcamp.co.kr>
2023-08-24 17:44:20 +0900

    I am adding a main function to investigate whether the issue, where the test code runs fine but it doesn't work as expected when used with the library, is due to the absence of the main function.

---
+++ app/src/main/java/io/github/mainmethod0126/annotation/scanner/App.java
@@ -0,0 +1,7 @@
+package io.github.mainmethod0126.annotation.scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Annotation Scanner App!");
+    }
+}
