21998b97004487657c5a3e5d8229bf14f7fef1b5
wusubsin <mainmethod0126@gmail.com>
2023-10-11 00:59:39 +0900

    - Delete unnecessary main function and App Class - Add gradle.properties file for maven-central deployment - Modify build.gradle due to the addition of the Gradle plugin for maven-central deployment

--- app/src/main/java/io/github/mainmethod0126/annotation/scanner/App.java
+++
@@ -1,7 +0,0 @@
-package io.github.mainmethod0126.annotation.scanner;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Annotation Scanner App!");
-    }
-}
